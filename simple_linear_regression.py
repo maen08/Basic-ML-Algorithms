a6ec3b0635d9e11b2472b99a42ced602e27a7d93
Stanley <2001stany@gmail.com>
2021-02-26 20:18:41 +0300

    add: dataset separation-test and train

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,10 +1,14 @@
 import numpy as nm
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 # import dataset
 
 dataset = pd.read_csv('./salary.csv')
-
 X = dataset.iloc[:,:-1].values
-Y = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:,:-1].values
+
+
+# separate data into test and train
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1/3, random_state = 0)
