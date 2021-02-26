fe82f2554e896818bdc784e433ba573d2811368f
Stanley <2001stany@gmail.com>
2021-02-26 18:10:36 +0300

    file added

--- decorators.py
+++ decorators.py
@@ -1,1 +1,35 @@
-import 
+import functools
+
+
+user = {
+    'name': 'Boby',
+    'access':'admin'
+}
+
+
+def admin_required(func):
+
+    @functools.wraps(func)
+    def is_required(*args, **kwargs):
+        if user['access'] == 'admin':
+            return func(*args, **kwargs)
+
+    return is_required
+        
+
+
+
+@admin_required
+def the_admin():
+    return 'THIS IS ADMIN'
+
+
+@admin_required
+def the_user():
+    return 'THIS IS USER'
+
+
+
+
+print(the_admin('admin'))
+# print(the_user('user'))
